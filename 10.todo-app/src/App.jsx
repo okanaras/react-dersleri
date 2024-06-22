db9ce583202410ea45f1a65a61f0101e8c6cd928
Okan Aras <oknaras@icloud.com>
2024-06-23 00:31:15 +0300

    todo app 5 - update todo

--- 10.todo-app/src/App.jsx
+++ 10.todo-app/src/App.jsx
@@ -10,19 +10,30 @@ function App() {
     setTodos([...todos, newTodo]);
   };
 
-  console.log(todos);
+  console.log('todos', todos);
 
   const removeTodo = (todoId) => {
     const extractedArr = todos.filter((todo) => todo.id !== todoId);
     setTodos([...extractedArr]);
-    console.log(extractedArr);
+    console.log('extractedArr', extractedArr);
   }
+
+  const updateTodo = (newTodo) => {
+    const updateTodos = todos.map(todo => {
+      if (todo.id !== newTodo.id) return todo
+      return newTodo;
+    });
+
+    setTodos([...updateTodos]);
+    console.log('updateTodos', updateTodos);
+  };
+
 
   return (
     <div className='App'>
       <div className='main'>
         <TodoCreate onCreateTodo={createTodo} />
-        <TodoList todos={todos} onRemoveTodo={removeTodo} />
+        <TodoList todos={todos} onRemoveTodo={removeTodo} onUpdateTodo={updateTodo} />
       </div>
     </div>
   )
--- 10.todo-app/src/components/Todo.jsx
+++ 10.todo-app/src/components/Todo.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdOutlineClear } from "react-icons/md";
 import { MdModeEdit } from "react-icons/md"; import '../App.css';
+import { FaCheck } from "react-icons/fa";
 
-function Todo({ todo, okan }) {
+function Todo({ todo, okan, onUpdateTodo }) {
     const { id, content } = todo;
+
+    const [editable, setEditable] = useState(false);
+    const [newTodo, setNewTodo] = useState(content);
 
     const removeTodo = () => {
         okan(id);
     };
 
+    const updateTodo = () => {
+
+        const request = {
+            id: id,
+            content: newTodo
+        };
+
+        onUpdateTodo(request);
+        setEditable(false);
+    };
+
     return (
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: "space-between", border: '1px solid lightgray', padding: '10px', marginTop: '10px' }}>
-            <div>{content}</div>
+            <div>
+                {
+                    editable ? <input type='text' className='todo-input' style={{ width: '380px' }} value={newTodo} onChange={(e) => setNewTodo(e.target.value)} /> : content
+                }
+            </div>
             <div>
                 <MdOutlineClear className='todo-icons' onClick={removeTodo} />
-                <MdModeEdit className='todo-icons' />
+                {
+                    editable ? <FaCheck className='todo-icons' onClick={updateTodo} /> : <MdModeEdit className='todo-icons' onClick={() => setEditable(true)} />
+                }
             </div>
         </div>
     )
